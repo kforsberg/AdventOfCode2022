cba3333e3ba05d20b8d7c599272ae1d62af24ed7
Kyle Forsberg <kforsberg16@gmail.com>
2022-12-08 10:57:27 -0500

    Part 1 complete

--- AdventOfCode.Day8/Application.cs
+++ AdventOfCode.Day8/Application.cs
@@ -11,14 +11,7 @@ class Application
 
     public void Run()
     {
-        for (int i = 0; i < TreeData.GetLength(0); i++)
-        {
-            for (int j = 0; j < TreeData.GetLength(1); j++)
-            {
-                Console.Write(TreeData[i,j]);
-            }
-            Console.WriteLine();
-        }
+        Console.WriteLine(SumVisibleTrees());
     }
 
     private void InitializeData(string[] lines)
@@ -34,6 +27,78 @@ class Application
             });
             yIndex++;
         }
+    }
+
+    private int SumVisibleTrees()
+    {
+        int totalVisible = 0;
+
+        for (int row = 0; row < TreeData.GetLength(0); row++)
+        {
+            for (int column = 0; column < TreeData.GetLength(1); column++)
+            {
+                if (IsVisibleDown(row, column) || IsVisibleUp(row, column) || IsVisibleLeft(row, column)|| IsVisibleRight(row, column) ) // todo: down
+                {
+                    totalVisible++;
+                }
+            }
+        }
+
+        return totalVisible;
+    }
+
+    // validated
+    public bool IsVisibleRight(int currentRow, int currentColumn)
+    {
+        var value = TreeData[currentRow, currentColumn];
+        var rowLength = TreeData.GetLength(1);
+        var row = Enumerable.Range(0, rowLength).Select(x => TreeData[currentRow, x]).ToArray().Skip(currentColumn + 1).Take(rowLength).ToList();
+        
+        if (row.Count() == 0)
+        {
+            return true;
+        }
+        return row.All(r => r < value);
+    }
+
+    // validated
+    public bool IsVisibleLeft(int currentRow, int currentColumn)
+    {
+        var value = TreeData[currentRow, currentColumn];
+        var rowLength = TreeData.GetLength(1);
+        var row = Enumerable.Range(0, rowLength).Select(x => TreeData[currentRow, x]).ToArray().Take(currentColumn).ToList();
+        if (row.Count() == 0)
+        {
+            return true;
+        }
+        return row.All(r => r < value);
+    }
+
+    // validated
+    private bool IsVisibleDown(int currentRow, int currentColumn)
+    {
+        var value = TreeData[currentRow, currentColumn];
+        var columnLength = TreeData.GetLength(0);
+        var column = Enumerable.Range(0, columnLength).Select(x => TreeData[x, currentColumn]).ToArray().Skip(currentRow + 1).Take(columnLength).ToList();
+        if (column.Count() == 0)
+        {
+            return true;
+        }
+        return column.All(c => c < value);
+    }
+
+    // in progress
+    private bool IsVisibleUp(int currentRow, int currentColumn)
+    {
+        var value = TreeData[currentRow, currentColumn];
+        var columnLength = TreeData.GetLength(0);
+        var column = Enumerable.Range(0, columnLength).Select(x => TreeData[x, currentColumn]).ToArray().Take(currentRow).ToList();
+        if (column.Count() == 0)
+        {
+            return true;
+        }
+
+        return column.All(c => c < value);
     }
 
 }
